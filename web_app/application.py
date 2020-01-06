2a478a70d196f3778c6776a348d51c6fd379cac2
matthewmoorcroft <matthewmoorcroft@gmail.com>
2020-01-06 18:45:59 +0100

    bugfix

--- web_app/application.py
+++ web_app/application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 @application.route('/')
 @application.route('/index')
 def index():
-    return render_template('index.html)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
