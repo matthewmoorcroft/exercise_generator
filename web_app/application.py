e860b571967f42907f9f537f079731ab9694ec73
matthewmoorcroft <matthewmoorcroft@gmail.com>
2020-01-12 18:58:17 +0100

    API set

--- web_app/application.py
+++ web_app/application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 application = Flask(__name__)
 
 
@@ -6,6 +7,16 @@ application = Flask(__name__)
 @application.route('/index')
 def index():
     return render_template('index.html')
+
+
+@application.route('/generator')
+def generator():
+    return requests.get(url="workout-gen-pdf.eu-west-2.elasticbeanstalk.com/generator")
+
+
+@application.route('/exercisehtml')
+def generator():
+    return requests.get(url="workout-gen-pdf.eu-west-2.elasticbeanstalk.com/exercisehtml")
 
 
 if __name__ == '__main__':
