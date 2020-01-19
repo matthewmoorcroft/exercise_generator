95206e3dfd903dd89f3a456004e5e3ba61feeaae
matthewmoorcroft <matthewmoorcroft@gmail.com>
2020-01-19 18:55:46 +0100

    Upload

--- exercise_gen_app/application.py
+++ exercise_gen_app/application.py
@@ -59,53 +59,6 @@ def generator():
     return response
 
 
-@application.route('/html')
-# @execution_time
-def generator_html():
-
-    # t_start = time()
-    # with open('exercises.json', 'r') as json_file:
-    #     exercises = json.load(json_file)
-    plan = exercise_generator.exercise_builder("4")
-    # t_end = time()
-    days = len(plan[0])
-    # pdf.output("tutorial.pdf")
-    html = render_template('exercises.html', days=days, exercises=plan)
-
-    return html
-
-
-@application.route('/exercisehtml')
-# @execution_time
-def exercise_htm():
-
-    t_start = time()
-    with open('exercises.json', 'r') as json_file:
-        exercises = json.load(json_file)
-    t_end = time()
-    days = len(exercises[0])
-    # pdf.output("tutorial.pdf")
-    html = render_template('exercises.html', days=days, exercises=exercises)
-    # path_wkhtmltopdf = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
-    path_wkhtmltopdf = "/usr/local/bin/wkhtmltopdf"
-    options = {
-        'page-size': 'A4',
-        'margin-top': '0in',
-        'margin-right': '0in',
-        'margin-bottom': '0in',
-        'margin-left': '0in',
-        'encoding': "UTF-8",
-        'no-outline': None,
-        'disable-smart-shrinking': ''
-    }
-    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-    pdf = pdfkit.from_string(html, "out.pdf", configuration=config, options=options)
-    response = make_response(pdf.output(dest='S').encode('latin-1'))
-    response.headers.set('Content-Disposition', 'attachment', filename='tutorial.pdf')
-    response.headers.set('Content-Type', 'applicationlication/pdf')
-    return response
-
-
 if __name__ == "__main__":
 
     application.run(port=5001)
--- web_app/application.py
+++ web_app/application.py
@@ -18,24 +18,5 @@ def generator():
     return response
 
 
-@application.route('/generator_one')
-def generator_one():
-    pdf = requests.get(url="http://172.31.16.8/generator").content
-    response = make_response(pdf)
-    response.headers.set('Content-Disposition', 'attachment', filename='tutorial.pdf')
-    response.headers.set('Content-Type', 'applicationlication/pdf')
-    return requests.get(url="http://172.31.16.8/generator")
-
-
-@application.route('/exercisehtml')
-def exercisehtml():
-    # return requests.get(url="http://workout-gen-pdf.eu-west-2.elasticbeanstalk.com/exercisehtml").content
-    pdf = requests.get(url="http://172.31.16.8/exercisehtml").content
-    response = make_response(pdf)
-    response.headers.set('Content-Disposition', 'attachment', filename='tutorial.pdf')
-    response.headers.set('Content-Type', 'applicationlication/pdf')
-    return response
-
-
 if __name__ == '__main__':
     application.run()
